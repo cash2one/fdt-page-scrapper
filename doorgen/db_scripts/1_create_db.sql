8f6299d627620eaf8784fc0da318c4ed821511a0
sidorenko.s.a <sidorenko.s.a@gmail.com>
2013-05-16 19:07:28 +0000

    Добавление таблицы сниппетов.

--- doorgen/db_scripts/1_create_db.sql
+++ doorgen/db_scripts/1_create_db.sql
@@ -65,6 +65,18 @@ CREATE TABLE IF NOT EXISTS `doorgen_banks`.`cached_page` (
 	cached_time TIMESTAMP
 ) Type=InnoDB;
 
+CREATE TABLE IF NOT EXISTS `doorgen_banks`.`snippets` (
+	snippets_page_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	snippets_index INT NOT NULL,
+	cached_page_id INT NOT NULL,
+	snippets_title VARCHAR(1024),
+	snippets_content VARCHAR(1024),
+	snippets_image_large VARCHAR(1024),
+	snippets_image_small VARCHAR(1024),
+	created_time TIMESTAMP,
+	CONSTRAINT FOREIGN KEY (`cached_page_id`) REFERENCES `cached_page` (`cached_page_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) Type=InnoDB;
+
 CREATE FUNCTION `doorgen_banks`.`encodestring` (input_string VARCHAR(200))
 RETURNS VARCHAR(255) DETERMINISTIC
 RETURN REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER(input_string),'а','a'),'б','b'),'в','v'),'г','g'),'д','d'),'е','e'),'ё','e'),'ж','j'),'з','z'),'и','i'),'й','y'),'к','k'),'л','l'),'м','m'),'н','n'),'о','o'),'п','p'),'р','r'),'с','s'),'т','t'),'у','u'),'ф','f'),'х','h'),'ц','ts'),'ч','ch'),'ш','sh'),'щ','sch'),'ъ','y'),'ы','yi'),'ь',''),'э','e'),'ю','yu'),'я','ya');
