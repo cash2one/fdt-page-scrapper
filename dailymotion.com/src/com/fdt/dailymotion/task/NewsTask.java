13fef8676013f774327526d7f6ec79f201184c03
sidorenko.s.a <sidorenko.s.a@gmail.com>
2014-11-14 14:24:03 +0000

    Add image upload and image posting info.

--- dailymotion.com/src/com/fdt/dailymotion/task/NewsTask.java
+++ dailymotion.com/src/com/fdt/dailymotion/task/NewsTask.java
@@ -1,19 +1,27 @@
 package com.fdt.dailymotion.task;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
 
 
 public class NewsTask{
 	
-	private File inputFileName;
-	
-	private String videoTitle = "Video title";
+	private static final Logger log = Logger.getLogger(NewsTask.class);
+
+	private File inputFile;
+
+	private String videoTitle = "";
 	private String videoid = "";
 	private String imageUrl = "";
 
 	private String key = "";
-	private String snippets = "Desc";
-	
+	private String snippets = "";
+
 	private String uploadUrl = "";
 
 	private int attempsCount = 1;
@@ -21,9 +29,63 @@ public class NewsTask{
 
 	public NewsTask(File inputFileName) {
 		super();
-		this.inputFileName = inputFileName;
-		
+		this.inputFile = inputFileName;
 		//TODO Read and parse file
+		parseFile();
+	}
+
+	private void parseFile(){
+		//read account list
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		StringBuilder fileAsStr = new StringBuilder();
+		
+		try {
+			int lineIndex = 0;
+			fr = new FileReader(inputFile);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+			while(line != null){
+				fileAsStr.append(line);
+				if(!"".equals(line)){
+					lineIndex++;
+					switch(lineIndex){
+					case 1:{
+						key = line.trim();
+						break;
+					}
+					case 2:{
+						videoTitle = line.trim();
+						break;
+					}
+					}
+				}
+			}
+			
+			//TODO Get image url
+			//TODO Get link for description
+			//TODO Generate description
+			
+		} catch (FileNotFoundException e) {
+			log.error("Reading PROPERTIES file: FileNotFoundException exception occured",e);
+		} catch (IOException e) {
+			log.error("Reading PROPERTIES file: IOException exception occured", e);
+		} finally {
+			try {
+				if(br != null)
+					br.close();
+			} catch (Throwable e) {
+				log.warn("Error while initializtion", e);
+			}
+			try {
+				if(fr != null)
+					fr.close();
+			} catch (Throwable e) {
+				log.warn("Error while initializtion", e);
+			}
+		}
 	}
 
 	public int getAttempsCount() {
@@ -50,7 +112,7 @@ public class NewsTask{
 	}
 
 	public File getInputFileName() {
-		return inputFileName;
+		return inputFile;
 	}
 
 	public String getVideoid() {
@@ -76,7 +138,7 @@ public class NewsTask{
 	public String getTags() {
 		return videoTitle;
 	}
-	
+
 	public String getDescription() {
 		return videoTitle;
 	}
