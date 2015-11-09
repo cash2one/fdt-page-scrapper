e14caacec630a05211755a6399f2b72875773c01
Sergey Sidorenko <sidorenko.s.a@gmail.com>
2015-11-09 14:42:56 +0300

    Add intothemoonrain.com site

---
+++ sites/intothemoonrain.in/index.php
@@ -0,0 +1,7 @@
+<?php
+/* Redirect browser */
+header("Location: http://intothemoonrain.in/readbook.php?asin=READ%20OR%20DOWNLOAD%20Book");
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
---
+++ sites/intothemoonrain.in/www/js.js
@@ -0,0 +1,15 @@
+	(function(d){
+	  var l = d.getElementById("foxyform_embed_link_682491");
+	      i = d.createElement('IFRAME');
+	  i.src = "http://www.foxyform.com/form.php?id=682491&sec_hash=c1967e37c62";
+	  i.width = "350px";
+	  i.height = "427px";
+	  i.frameborder = 0;
+	  i.setAttribute('style', 'border: none;');
+	  i.setAttribute('frameborder', '0');
+	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+	  l.parentNode.insertBefore(i,l);
+	})(document);
+
+
+		
