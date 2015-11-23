47bb02c511e99e55ae0f2b0cba1823aa1093db1d
Sergey Sidorenko <sidorenko.s.a@gmail.com>
2015-11-23 17:55:21 +0300

    add

---
+++ snippet_utils/snippet_util_scrapper.bat
@@ -0,0 +1,2 @@
+title "Anchor Title Scrapper v1.0"
+java -cp "../java/*;deploy/snippet_utils.jar" com.fdt.snippetutils.AnchorTitleScrapper cfg_anchor_scrapper.ini
---
+++ snippet_utils/snippet_util_shorturl_inserter.bat
@@ -0,0 +1,2 @@
+title "Anchor Title Replacer v1.0"
+java -cp "../java/*;deploy/snippet_utils.jar" com.fdt.snippetutils.KeywordShortUrlInserter cfg_short_url_inserter.ini
