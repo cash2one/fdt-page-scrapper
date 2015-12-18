5fc3172e25c86c24fe6872db60307512b8fc7047
Sergey Sidorenko <sidorenko.s.a@gmail.com>
2015-12-18 22:48:44 +0300

    ccomiit

---
+++ doorgen_poller/door_gen_poster.sh
@@ -0,0 +1,2 @@
+title "Doorgen poster v1.0"
+java -cp  "../java/*" com.fdt.doorgen.key.pooler.DoorgenPosterRunner config.ini
---
+++ doorgen_poller/door_gen_update.sh
@@ -0,0 +1,2 @@
+title "Doorgen Updater v1.0"
+java -cp  "../java/*" com.fdt.multisnipgen.scrapper.DoorgenUpdaterRunner config.ini
